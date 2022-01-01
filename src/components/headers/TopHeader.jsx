1f40743e3fed691670a494c2ef410b0116ad3312
Hanan <hanan.b@getafixtechnologies.com>
2022-01-01 16:49:17 +0530

    Banner UI and API

--- src/components/headers/TopHeader.jsx
+++ src/components/headers/TopHeader.jsx
@@ -1,67 +1,89 @@
 const TopHeader = () => {
-  return (
-    <div class="header-top hidden-xs hidden-sm">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="hel-l">
-              <nav class="secondary-navigation herald-menu">
-                <ul id="menu-herald-secondary-1" class="menu">
-                  <li
-                    id="menu-item-1365"
-                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1365"
-                  >
-                    <a href="https://demo.mekshq.com/herald/?page_id=872">
-                      Layouts
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-1137"
-                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1137"
-                  >
-                    <a href="https://demo.mekshq.com/herald/?page_id=328">
-                      Styleguide
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-1135"
-                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1135"
-                  >
-                    <a href="https://demo.mekshq.com/herald/?page_id=294">
-                      Contact
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-1693"
-                    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1693"
-                  >
-                    <a href="https://demo.mekshq.com/herald/?post_type=product">
-                      Shop
-                    </a>
-                  </li>
-                  <li
-                    id="menu-item-1694"
-                    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1694"
-                  >
-                    <a href="https://demo.mekshq.com/herald/?forum=forum">
-                      Forum
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+    return (
+        <div className="header-top hidden-xs hidden-sm">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="hel-l">
+                <nav className="secondary-navigation herald-menu">
+                  <ul id="menu-herald-secondary-1" className="menu">
+                    <li
+                      id="menu-item-1365"
+                      className="
+                        menu-item
+                        menu-item-type-post_type
+                        menu-item-object-page menu-item-1365
+                      "
+                    >
+                      <a href="https://demo.mekshq.com/herald/?page_id=872"
+                        >Layouts</a
+                      >
+                    </li>
+                    <li
+                      id="menu-item-1137"
+                      className="
+                        menu-item
+                        menu-item-type-post_type
+                        menu-item-object-page menu-item-1137
+                      "
+                    >
+                      <a href="https://demo.mekshq.com/herald/?page_id=328"
+                        >Styleguide</a
+                      >
+                    </li>
+                    <li
+                      id="menu-item-1135"
+                      className="
+                        menu-item
+                        menu-item-type-post_type
+                        menu-item-object-page menu-item-1135
+                      "
+                    >
+                      <a href="https://demo.mekshq.com/herald/?page_id=294"
+                        >Contact</a
+                      >
+                    </li>
+                    <li
+                      id="menu-item-1693"
+                      className="
+                        menu-item
+                        menu-item-type-custom
+                        menu-item-object-custom
+                        menu-item-1693
+                      "
+                    >
+                      <a
+                        href="https://demo.mekshq.com/herald/?post_type=product"
+                        >Shop</a
+                      >
+                    </li>
+                    <li
+                      id="menu-item-1694"
+                      className="
+                        menu-item
+                        menu-item-type-custom
+                        menu-item-object-custom
+                        menu-item-1694
+                      "
+                    >
+                      <a href="https://demo.mekshq.com/herald/?forum=forum"
+                        >Forum</a
+                      >
+                    </li>
+                  </ul>
+                </nav>
+              </div>
 
-            <div class="hel-r">
-              <span class="herald-calendar">
-                <i class="fa fa-calendar"></i>
-                December 30, 2021
-              </span>
+              <div className="hel-r">
+                <span className="herald-calendar"
+                  ><i className="fa fa-calendar"></i>January 1, 2022</span
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default TopHeader
--- src/components/headers/TrendingHeader.jsx
+++ src/components/headers/TrendingHeader.jsx
@@ -1,196 +1,171 @@
 const TrendingHeader = () => {
-  return (
-    <div class="header-trending hidden-xs hidden-sm">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-md-12">
-            <div class="row" data-col="6">
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=203"
-                    title="Grandma&#8217;s secret blueberry pie recipe revealed!"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-600x600.jpg 600w
+    return (
+        <div className="header-trending hidden-xs hidden-sm">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-md-12">
+              <div className="row" data-col="6">
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=203"
+                      title="Grandma&#8217;s secret blueberry pie recipe revealed!"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald024-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                        
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=203"
+                      >Grandma&#8217;s secret blueberry pie recipe revealed!</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=203">
-                    Grandma&#8217;s secret blueberry pie recipe revealed!
-                  </a>
-                </h4>
-              </div>
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=180"
-                    title="Little known facts about football and why they matter"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-600x600.jpg 600w
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=180"
+                      title="Little known facts about football and why they matter"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald018-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                         
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=180"
+                      >Little known facts about football and why they matter</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=180">
-                    Little known facts about football and why they matter
-                  </a>
-                </h4>
-              </div>
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=197"
-                    title="Learn to play golf by practicing 15 minutes a day"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-600x600.jpg 600w
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=197"
+                      title="Learn to play golf by practicing 15 minutes a day"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald081-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                         
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=197"
+                      >Learn to play golf by practicing 15 minutes a day</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=197">
-                    Learn to play golf by practicing 15 minutes a day
-                  </a>
-                </h4>
-              </div>
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=192"
-                    title="Everything you ever need to known about scarves"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-600x600.jpg 600w
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=192"
+                      title="Everything you ever need to known about scarves"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald092-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                         
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=192"
+                      >Everything you ever need to known about scarves</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=192">
-                    Everything you ever need to known about scarves
-                  </a>
-                </h4>
-              </div>
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=191"
-                    title="Pasta is the secret ingredient for a healthy lifestyle"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-600x600.jpg 600w
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=191"
+                      title="Pasta is the secret ingredient for a healthy lifestyle"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/11/herald119-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                         
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=191"
+                      >Pasta is the secret ingredient for a healthy lifestyle</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=191">
-                    Pasta is the secret ingredient for a healthy lifestyle
-                  </a>
-                </h4>
-              </div>
-              <div class="col-lg-2 col-md-2">
-                <div class="herald-post-thumbnail">
-                  <a
-                    href="https://demo.mekshq.com/herald/?p=174"
-                    title="This is how coffee can help you predict the future"
-                  >
-                    <img
-                      width="150"
-                      height="150"
-                      src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-150x150.jpg"
-                      class="attachment-thumbnail size-thumbnail wp-post-image"
-                      alt=""
-                      loading="lazy"
-                      srcset="
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-150x150.jpg 150w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-65x65.jpg    65w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-180x180.jpg 180w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-300x300.jpg 300w,
-                          https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-600x600.jpg 600w
+                <div className="col-lg-2 col-md-2">
+                  <div className="herald-post-thumbnail">
+                    <a
+                      href="https://demo.mekshq.com/herald/?p=174"
+                      title="This is how coffee can help you predict the future"
+                      ><img
+                        width="150"
+                        height="150"
+                        src="https://mksdmcdn-9b59.kxcdn.com/herald/wp-content/uploads/2015/05/herald034-e1447696639921-150x150.jpg"
+                        className="
+                          attachment-thumbnail
+                          size-thumbnail
+                          wp-post-image
                         "
-                      sizes="(max-width: 150px) 100vw, 150px"
-                    />
-                  </a>
+                        alt=""
+                        loading="lazy"
+                        sizes="(max-width: 150px) 100vw, 150px"
+                    /></a>
+                  </div>
+                  <h4 className="h6">
+                    <a href="https://demo.mekshq.com/herald/?p=174"
+                      >This is how coffee can help you predict the future</a
+                    >
+                  </h4>
                 </div>
-                <h4 class="h6">
-                  <a href="https://demo.mekshq.com/herald/?p=174">
-                    This is how coffee can help you predict the future
-                  </a>
-                </h4>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default TrendingHeader
